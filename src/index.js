ea01a61cadf1bd19785899f1c4b586fba045300f
atielijohn8 <atielijohn4@gmail.com>
2025-07-07 02:38:00 +0300

    intialize api & dom elements ref

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// API configuration
+const API_URL = 'http://localhost:3000/posts';
+
+// DOM elements
+const postList = document.getElementById('post-list');
+const postDetail = document.querySelector('#post-detail .post-card');
+const form = document.getElementById('new-post-form');
+const cancelBtn = document.querySelector('.cancel-btn');
+
